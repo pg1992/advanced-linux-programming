7a307ca3eb38b682e45adfc350d6d98ee23b1895
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-04-11 17:27:01 -0300

    Add some comments

--- appb/create-file.c
+++ appb/create-file.c
@@ -7,12 +7,16 @@
 
 int main(int argc, char **argv)
 {
+    /* The path at which to create a new file. */
     char *path = argv[1];
+    /* The permissions of the new file. */
     mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
+    /* Create the file. */
     int fd = open(path, O_WRONLY | O_EXCL | O_CREAT, mode);
 
     if (fd == -1) {
+        /* An error occurred.  Print an error message and bail. */
         perror("open");
         return 1;
     }

531f2f45aa7d436b2c4e3bdf248f0fdd4247f4c7
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-04-11 21:07:48 -0300

    Include a `hexdump` clone as an example of `read`

---
+++ appb/hexdump.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    unsigned char buffer[16];
+    size_t offset = 0;
+    size_t bytes_read;
+    int i;
+
+    int fd = open(argv[1], O_RDONLY);
+
+    do {
+        bytes_read = read(fd, buffer, sizeof(buffer));
+        printf("0x%06x: ", offset);
+        for (i = 0; i < bytes_read; i++)
+            printf("%02x ", buffer[i]);
+        printf("\n");
+        offset += bytes_read;
+    } while(bytes_read == sizeof(buffer));
+
+    return 0;
+}

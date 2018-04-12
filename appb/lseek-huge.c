68bd6b7e7f3ef2eb26c9ece224286831ff7489d6
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-04-12 09:59:45 -0300

    Explain each step with comments

--- appb/lseek-huge.c
+++ appb/lseek-huge.c
@@ -11,9 +11,13 @@ int main(int argc, char **argv)
     char *filename = argv[1];
     size_t length = (size_t) atoi(argv[2]) * megabyte;
 
+    /* Open a new file. */
     int fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
+    /* Jump to 1 byte short of where we want the file to end. */
     lseek(fd, length - 1, SEEK_SET);
+    /* Write a single 0 byte. */
     write(fd, &zero, 1);
+    /* All done. */
     close(fd);
 
     return 0;

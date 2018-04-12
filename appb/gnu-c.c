b20004f3e35358a8089c518865d7bd20c5740383
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-04-12 12:24:19 -0300

    Explain each step of `gnu-c.c`

--- appb/gnu-c.c
+++ appb/gnu-c.c
@@ -8,28 +8,39 @@
 #include <stdio.h>
 #include <string.h>
 
+/* Size of each buffer. */
 #define BUFF_SIZE 20
+/* Size of struct iovec array. */
 #define VEC_SIZE 20
 
 int main(int argc, char **argv)
 {
+    /* The first argument is the name of the file. */
     char *filename = argv[1];
+    /* Open the file using the Standard C Library I/O functions. */
     FILE *fp = fopen(filename, "r");
+    /* Get the file descriptor from the file stream. */
     int fd = fileno(fp);
     struct iovec *vec, *vec_next;
+    /* Create an array of buffers. */
     char buffer[VEC_SIZE][BUFF_SIZE + 1];
     int i;
 
+    /* Specify each buffer address and size inside of an array of
+     * struct iovec. */
     vec = (struct iovec *) malloc(VEC_SIZE * sizeof(struct iovec));
     for (i = 0, vec_next = vec; i < VEC_SIZE; i++, vec_next++) {
         vec_next->iov_base = buffer[i];
         vec_next->iov_len = BUFF_SIZE;
-        buffer[i][BUFF_SIZE] = 0;
+        /* End each buffer with a NUL char so that it prints nicely. */
+        buffer[i][BUFF_SIZE] = '\0';
     }
 
+    /* Read the contents of the file into the vector. */
     readv(fd, vec, VEC_SIZE);
     close(fd);
 
+    /* Print each buffer. */
     for (i = 0; i < VEC_SIZE; i++)
         printf("*** %s ***\n", buffer[i]);
 

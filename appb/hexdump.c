600cd4b4a259cb9a213a730396b0a92e00e0eefb
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-04-11 21:12:03 -0300

    Explain the steps in `hexdump.c`

--- appb/hexdump.c
+++ appb/hexdump.c
@@ -11,16 +11,24 @@ int main(int argc, char **argv)
     size_t bytes_read;
     int i;
 
+    /* Open the file for reading. */
     int fd = open(argv[1], O_RDONLY);
 
+    /* Read from the file, one chunk at a time.  Continue until read
+     * "comes up short", that is, reads less than we asked for.
+     * This indicates that we've hit the end of the file. */
     do {
+        /* Read the next line's worth of bytes. */
         bytes_read = read(fd, buffer, sizeof(buffer));
+        /* Print the offset in the file, followed by the bytes themselves. */
         printf("0x%06x: ", offset);
         for (i = 0; i < bytes_read; i++)
             printf("%02x ", buffer[i]);
         printf("\n");
+        /* Keep count of our position in the file. */
         offset += bytes_read;
     } while(bytes_read == sizeof(buffer));
 
+    /* All done. */
     return 0;
 }

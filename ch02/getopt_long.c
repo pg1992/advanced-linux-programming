ed84bbda09de3dbe674b7895d788a7d48a17bd57
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-04-19 22:59:31 -0300

    Explain each step and options

--- ch02/getopt_long.c
+++ ch02/getopt_long.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 #include <getopt.h>
 
+/* The name of this program. */
 const char* program_name;
+
+/* Prints usage information for this program to STREAM (typically
+ * stdout or stderr), and exit the program with EXIT_CODE.  Does not
+ * return. */
 
 void print_usage(FILE* stream, int exit_code)
 {
@@ -14,20 +19,31 @@ void print_usage(FILE* stream, int exit_code)
     exit(exit_code);
 }
 
+/* Main program entry point.  ARGC contains the number of argument list
+ * elements; ARGV is an array of pointers to them. */
+
 int main(int argc, char* argv[])
 {
     int next_option;
 
+    /* A string listing valid short option letters. */
     const char* const short_options = "ho:v";
+    /* An array describing valid long options. */
     const struct option long_options[] = {
         { "help",    0, NULL, 'h' },
         { "output",  1, NULL, 'o' },
         { "verbose", 0, NULL, 'v' },
-        { NULL,      0, NULL,  0  }
+        { NULL,      0, NULL,  0  }  /* Required at end of array. */
     };
+
+    /* The name of the file to receive program output, or NULL for
+     * standard output. */
     const char* output_filename = NULL;
+    /* Whether to display verbose messages. */
     int verbose = 0;
 
+    /* Remember the name of the program, to incorporate in messages.
+     * The name is stored in argv[0]. */
     program_name = argv[0];
 
     do {
@@ -35,28 +51,40 @@ int main(int argc, char* argv[])
                                   long_options, NULL);
         switch (next_option)
         {
-            case 'h':
+            case 'h':   /* -h or --help */
+                /* User has requested usage information.  Print it to standard
+                 * output, and exit with exit code zero (normal termination). */
                 print_usage(stdout, 0);
-            case 'o':
+            case 'o':   /* -o or --output */
+                /* This option takes an argument, the name of the
+                 * output file.  */
                 output_filename = optarg;
                 break;
-            case 'v':
+            case 'v':   /* -v or --verbose */
                 verbose = 1;
                 break;
-            case '?':
+            case '?':   /* The user specified an invalid option. */
+                /* Print usage information to standard error, and exit with
+                 * exit code one (indicating abnormal termination). */
                 print_usage(stderr, 1);
-            case -1:
+            case -1:   /* Done with options. */
                 break;
-            default:
+            default:   /* Something else: unexpected. */
                 abort();
         }
     } while (next_option != -1);
+
+    /* Done with options.  OPTIND points to first nonoption argument.
+     * For demonstration purposes, print them if the verbose option was
+     * specified. */
 
     if (verbose) {
         int i;
         for (i = optind; i < argc; ++i)
             printf("Argument: %s\n", argv[i]);
     }
+
+    /* The main program goes here. */
 
     exit(EXIT_SUCCESS);
 }

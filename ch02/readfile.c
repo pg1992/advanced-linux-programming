f3de20cf6216e9e590a3654222df9268099ba3b5
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-05-20 16:47:21 -0300

    Explain the steps along deallocation after error

--- ch02/readfile.c
+++ ch02/readfile.c
@@ -10,21 +10,26 @@ char *read_from_file(const char *filename, size_t length)
     int fd;
     ssize_t bytes_read;
 
+    /* Allocate the buffer. */
     buffer = (char *) malloc(length * sizeof(char));
     if (buffer == NULL)
         return NULL;
+    /* Open the file. */
     fd = open(filename, O_RDONLY);
     if (fd == -1) {
+        /* Open failed.  Deallocate buffer before returning. */
         free(buffer);
         return NULL;
     }
+    /* Read the data. */
     bytes_read = read(fd, buffer, length);
     if (bytes_read != length) {
+        /* Read failed.  Deallocate buffer and close fd before returning. */
         free(buffer);
         close(fd);
         return NULL;
     }
-
+    /* Everything's fine.  Close the file and return the buffer. */
     close(fd);
     return buffer;
 }

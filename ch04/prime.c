fcc462871daecd6cd892069b8e65f7940d09f182
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-05-27 20:33:57 -0300

    Explain the main steps

--- ch04/prime.c
+++ ch04/prime.c
@@ -1,6 +1,9 @@
 #include <pthread.h>
 #include <stdio.h>
 
+/* Compute successive prime numbers (very inefficiently).  Return the
+ * Nth prime number, where N is the value pointed to by *ARG. */
+
 void* compute_prime(void* arg)
 {
     long candidate = 2;
@@ -10,14 +13,18 @@ void* compute_prime(void* arg)
         int factor;
         int is_prime = 1;
 
+        /* Test primality by successive division. */
         for (factor = 2; factor < candidate; factor++)
             if (candidate % factor == 0) {
                 is_prime = 0;
                 break;
             }
 
+        /* Is this the prime number we're looking for? */
         if (is_prime)
             if (--n == 0)
+                /* Return the desired prime number as the thread
+                 * return value. */
                 return (void*) candidate;
 
         ++candidate;
@@ -32,9 +39,13 @@ int main(void)
     long which_prime = 5000;
     long prime;
 
+    /* Start the computing thread, up to the 5,000th prime number. */
     pthread_create(&thread, NULL, &compute_prime, (void*) &which_prime);
+    /* Do some other work here... */
+    /* Wait for the prime number thread to complete, and get the result. */
     pthread_join(thread, (void*) &prime);
 
+    /* Print the largest prime number it computed. */
     printf("The %dth prime number is %d,\n", which_prime, prime);
 
     return 0;

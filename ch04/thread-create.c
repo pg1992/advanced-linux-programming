b575ace3b7e0ac236ff2c0b73af171dbe8ffe39d
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-05-27 14:29:44 -0300

    Comment on the main steps of `thread-create.c`

--- ch04/thread-create.c
+++ ch04/thread-create.c
@@ -1,6 +1,8 @@
 #include <pthread.h>
 #include <stdio.h>
 
+/* Print o's to stderr.  The parameter is unused.  Does not return. */
+
 void *print_os(void *unused)
 {
     while (1)
@@ -8,10 +10,15 @@ void *print_os(void *unused)
     return NULL;
 }
 
+/* The main program. */
+
 int main()
 {
     pthread_t thread_id;
+    /* Create a new thread.  The new thread will run the print_os
+     * function. */
     pthread_create(&thread_id, NULL, &print_os, NULL);
+    /* Print -'s continuously to stderr. */
     while (1)
         fputc('-', stderr);
     return 0;

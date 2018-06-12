844e01a1b648b50a2a7497930eea830a1cf9f0e2
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-06-12 14:12:50 -0300

    Actually execute the C++ code

--- ch04/cxx-exit.cpp
+++ ch04/cxx-exit.cpp
@@ -1,4 +1,6 @@
 #include <pthread.h>
+#include <unistd.h>
+#include <cstdio>
 
 class ThreadExitException
 {
@@ -13,6 +15,7 @@ class ThreadExitException
          * constructor. */
         void* DoThreadExit()
         {
+            printf("Thread exited.\n");
             pthread_exit(thread_return_value_);
         }
 
@@ -21,10 +24,19 @@ class ThreadExitException
         void* thread_return_value_;
 };
 
+static bool exit_thread = false;
+
+bool should_exit_thread_immediately(void)
+{
+    return exit_thread;
+}
+
 void do_some_work()
 {
     while (1) {
         /* Do some useful things here... */
+        printf("hey...\n");
+        usleep(50000);
         if (should_exit_thread_immediately())
             throw ThreadExitException(/* thread's return value = */ NULL);
     }
@@ -37,7 +49,20 @@ void* thread_function(void*)
     }
     catch (ThreadExitException ex) {
         /* Some function indicated that we should exit the thread. */
+        printf("Exception thrown.\n");
         ex.DoThreadExit();
     }
     return NULL;
+}
+
+int main(void)
+{
+    pthread_t tid;
+    pthread_create(&tid, NULL, thread_function, NULL);
+    for (int i = 0; i < 10; i++) {
+        printf("hello...\n");
+        usleep(50000);
+    }
+    exit_thread = true;
+    pthread_join(tid, NULL);
 }

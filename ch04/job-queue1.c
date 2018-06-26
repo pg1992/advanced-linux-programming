034bc8923b462a1880836e4d53c6500902d626d7
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-06-26 09:38:04 -0300

    Include an skeleton example of a race condition

---
+++ ch04/job-queue1.c
@@ -0,0 +1,18 @@
+#include <malloc.h>
+
+struct job {
+    struct job* next;
+};
+
+struct job* job_queue;
+
+void* thread_function(void* arg)
+{
+    while (job_queue != NULL) {
+        struct job* next_job = job_queue;
+        job_queue = job_queue->next;
+        process_job(next_job);
+        free(next_job);
+    }
+    return NULL;
+}

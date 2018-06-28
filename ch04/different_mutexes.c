22d78aa035135e917dce3f97a5f367dec6f7a236
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-06-28 00:10:41 -0300

    Remove code duplication

--- ch04/different_mutexes.c
+++ ch04/different_mutexes.c
@@ -10,38 +10,31 @@ pthread_mutexattr_t mutex_attr;
 
 void *thread_task(void *arg) {
     long tid = (long) arg;
-    int ret;
+    int ret, i;
+    const int num_locks = 5;
 
     pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 
     fprintf(stderr, "Thread %ld started.\n", tid);
-    fprintf(stderr, "Thread %ld will try to get first lock.\n", tid);
-    ret = pthread_mutex_lock(&mutex);
-    if (ret == 0)
-        fprintf(stderr, "Thread %ld got first lock.\n", tid);
-    else {
-        switch(ret) {
-            case EDEADLK:
-                fprintf(stderr, "Thread %ld: %s\n", tid, strerror(ret));
-                break;
-            default:
-                break;
+
+    for (i = 1; i <= num_locks; i++) {
+        fprintf(stderr, "Thread %ld:"
+                        " I will try to get lock number %i.\n", tid, i);
+        ret = pthread_mutex_lock(&mutex);
+        if (ret == 0)
+            fprintf(stderr, "Thread %ld: I got lock number %i.\n", tid, i);
+        else {
+            switch(ret) {
+                case EDEADLK:
+                    fprintf(stderr, "Thread %ld: %s\n", tid, strerror(ret));
+                    break;
+                default:
+                    break;
+            }
         }
     }
-    fprintf(stderr, "Thread %ld will try to get second lock.\n", tid);
-    ret = pthread_mutex_lock(&mutex);
-    if (ret == 0)
-        fprintf(stderr, "Thread %ld got second lock.\n", tid);
-    else {
-        switch(ret) {
-            case EDEADLK:
-                fprintf(stderr, "Thread %ld: %s\n", tid, strerror(ret));
-                break;
-            default:
-                break;
-        }
-    }
+
     fprintf(stderr, "Thread %ld finished.\n", tid);
 
     return NULL;
@@ -52,81 +45,42 @@ int main(int argc, char *argv[])
     pthread_t tid, other;
     pthread_attr_t attr;
     void *ret;
+    const char *lock_names[] = {"fast\0", "recursive\0", "error-checking\0"};
+    int lock_types[] = {
+        PTHREAD_MUTEX_NORMAL,
+        PTHREAD_MUTEX_RECURSIVE,
+        PTHREAD_MUTEX_ERRORCHECK
+    };
+    int i;
 
-    pthread_mutexattr_init(&mutex_attr);
-    pthread_mutexattr_settype(&mutex_attr, PTHREAD_MUTEX_NORMAL);
-    pthread_mutex_init(&mutex, &mutex_attr);
-    pthread_mutexattr_destroy(&mutex_attr);
+    for (i = 0; i < 3; i++) {
+        pthread_mutexattr_init(&mutex_attr);
+        pthread_mutexattr_settype(&mutex_attr, lock_types[i]);
+        pthread_mutex_init(&mutex, &mutex_attr);
+        pthread_mutexattr_destroy(&mutex_attr);
 
-    printf("Creating 2 threads that will lock a fast lock twice.\n");
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    pthread_create(&tid, &attr, thread_task, (void *) 1);
-    pthread_create(&other, &attr, thread_task, (void *) 2);
-    pthread_attr_destroy(&attr);
+        printf("Creating 2 threads that will lock a %s lock twice.\n", lock_names[i]);
+        pthread_attr_init(&attr);
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+        pthread_create(&tid, &attr, thread_task, (void *) 1);
+        pthread_create(&other, &attr, thread_task, (void *) 2);
+        pthread_attr_destroy(&attr);
 
-    printf("Sleeping for 5s.\n");
-    sleep(5);
-    pthread_cancel(tid);
-    pthread_cancel(other);
+        printf("Sleeping for 5s.\n");
+        sleep(5);
+        pthread_cancel(tid);
+        pthread_cancel(other);
 
-    pthread_mutex_destroy(&mutex);
-    pthread_join(tid, &ret);
-    if (ret == PTHREAD_CANCELED)
-        printf("First thread had to be cancelled.\n");
-    pthread_join(other, &ret);
-    if (ret == PTHREAD_CANCELED)
-        printf("Second thread had to be cancelled.\n");
+        pthread_mutex_destroy(&mutex);
+        pthread_join(tid, &ret);
+        if (ret == PTHREAD_CANCELED)
+            printf("First thread had to be cancelled.\n");
+        pthread_join(other, &ret);
+        if (ret == PTHREAD_CANCELED)
+            printf("Second thread had to be cancelled.\n");
 
-    pthread_mutexattr_init(&mutex_attr);
-    pthread_mutexattr_settype(&mutex_attr, PTHREAD_MUTEX_RECURSIVE);
-    pthread_mutex_init(&mutex, &mutex_attr);
-    pthread_mutexattr_destroy(&mutex_attr);
-
-    printf("\nCreating 2 threads that will lock a recursive lock twice.\n");
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    pthread_create(&tid, &attr, thread_task, (void *) 1);
-    pthread_create(&other, &attr, thread_task, (void *) 2);
-    pthread_attr_destroy(&attr);
-
-    printf("Sleeping for 5s.\n");
-    sleep(5);
-    pthread_cancel(tid);
-    pthread_cancel(other);
-
-    pthread_mutex_destroy(&mutex);
-    pthread_join(tid, &ret);
-    if (ret == PTHREAD_CANCELED)
-        printf("First thread had to be cancelled.\n");
-    pthread_join(other, &ret);
-    if (ret == PTHREAD_CANCELED)
-        printf("Second thread had to be cancelled.\n");
-
-    pthread_mutexattr_init(&mutex_attr);
-    pthread_mutexattr_settype(&mutex_attr, PTHREAD_MUTEX_ERRORCHECK);
-    pthread_mutex_init(&mutex, &mutex_attr);
-    pthread_mutexattr_destroy(&mutex_attr);
-
-    printf("\nCreating 2 threads that will lock a error-checking lock twice.\n");
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    pthread_create(&tid, &attr, thread_task, (void *) 1);
-    pthread_create(&other, &attr, thread_task, (void *) 2);
-    pthread_attr_destroy(&attr);
-
-    printf("Sleeping for 5s.\n");
-    sleep(5);
-    pthread_cancel(tid);
-    pthread_cancel(other);
-
-    pthread_mutex_destroy(&mutex);
-    pthread_join(tid, &ret);
-    if (ret == PTHREAD_CANCELED)
-        printf("First thread had to be cancelled.\n");
-    pthread_join(other, &ret);
-    if (ret == PTHREAD_CANCELED)
-        printf("Second thread had to be cancelled.\n");
+        printf("\n");
+    }
 
     return EXIT_SUCCESS;
 }

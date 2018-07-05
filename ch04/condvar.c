74be9669d18df514643a74dea60a7239b4e668b9
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-07-05 10:18:30 -0300

    Make the condition variable example runnable
    - Add the `main` procedure;
    - A thread to keep toggling the flag in parallel;
    - Make the global variables `static`;
    - Fill the `do_work` routine to simulate real work;
    - Output messages while doing work and when the thread will get blocked;
    - Add delays to facilitate visualization of results.

--- ch04/condvar.c
+++ ch04/condvar.c
@@ -1,8 +1,13 @@
 #include <pthread.h>
+#include <unistd.h>
+#include <string.h>
 
-int thread_flag;
-pthread_cond_t thread_flag_cv;
-pthread_mutex_t thread_flag_mutex;
+#define MAX_LOOPS 10
+
+static int thread_flag;
+static int loop_count = 0;
+static pthread_cond_t thread_flag_cv;
+static pthread_mutex_t thread_flag_mutex;
 
 void initialize_flag()
 {
@@ -13,25 +18,38 @@ void initialize_flag()
     thread_flag = 0;
 }
 
+/* Print a message to STDOUT to simulate some real work. */
+void do_work()
+{
+    const char *message = "I'm doing some work :)\n\0";
+    write(STDOUT_FILENO, message, strlen(message));
+}
+
 /* Calls do_work repeatedly while the thread flag is set; blocks if
  * the flag is clear. */
 
-void *thread_function(void* thread_arg)
+void *working_thread(void* thread_arg)
 {
     /* Loop infinitely. */
-    while(1) {
+    while(loop_count < MAX_LOOPS) {
         /* Lock the mutex before accessing the flag value. */
         pthread_mutex_lock(&thread_flag_mutex);
-        while(!thread_flag)
+        while(!thread_flag) {
+            /* Announce that it will be blocked since the flag is not set. */
+            const char *message = "Flag isn't set :(  [sleeping]\n\0";
+            write(STDOUT_FILENO, message, strlen(message));
             /* The flag is clear.  Wait for a signal on the condition
              * variable, indicating that the flag value has changed.  When the
              * signal arrives and this thread unblocks, loop and check the
              * flag again. */
             pthread_cond_wait(&thread_flag_cv, &thread_flag_mutex);
+        }
         /* When we've gotten here, we know the flag must be set.  Unlock
-         * the mutex.*/
+         * the mutex. */
         pthread_mutex_unlock(&thread_flag_mutex);
         do_work();
+        /* Sleep for half a second. */
+        usleep(500000);
     }
     return NULL;
 }
@@ -50,4 +68,29 @@ void set_thread_flag(int flag_value)
     pthread_cond_signal(&thread_flag_cv);
     /* Unlock the mutex. */
     pthread_mutex_unlock(&thread_flag_mutex);
+}
+
+/* Keep toggling the flag with a 1s period. */
+void *toggling_thread(void *thread_arg)
+{
+    while (loop_count++ < MAX_LOOPS) {
+        set_thread_flag(!thread_flag);
+        sleep(1);
+    }
+}
+
+int main(void)
+{
+    pthread_t toggle_tid, work_tid;
+
+    /* Start the threads. */
+    pthread_create(&work_tid, NULL, working_thread, NULL);
+    pthread_create(&toggle_tid, NULL, toggling_thread, NULL);
+
+    /* Join the threads (e.g., wait for them to finish). */
+    pthread_join(toggle_tid, NULL);
+    pthread_join(work_tid, NULL);
+
+    /* Good bye ;) */
+    return 0;
 }
